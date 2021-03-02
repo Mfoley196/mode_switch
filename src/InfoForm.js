8997075ad7cdf5da245e143f7ee6d062dd2257b5
M. Foley <mjfoley@uwaterloo.ca>
2021-03-02 14:15:07 -0500

    initial commit

---
+++ src/InfoForm.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const InfoForm = (props) => {
+  const {goToStage, pNo, setPNo} = props
+  
+  
+  const handleSubmit = (evt) => {
+      evt.preventDefault();
+      goToStage({type: 'instruction'});
+      console.log(pNo);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Participant Number:
+        <input
+          type="text"
+          onChange={e => setPNo(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
+export default InfoForm;
